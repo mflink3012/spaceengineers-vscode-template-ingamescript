4d0e3cfe9489a8dd5f168250f622b86ca30b9388
Marco L <mfl-git@bitblox.de>
2021-04-02 23:50:44 +0200

    Removed debugging-comment to save some space in script.

--- template/extract_script.ps1
+++ template/extract_script.ps1
@@ -11,13 +11,21 @@ if ($Debugging) {
     # Keep empty lines before body, because then you will have correct linenumbers on stacktraces and error-messages.
     [int] $newlines_count_header = $programcs.Substring(0, $programcs_header_end).Split("`n").Count - 1
     $programcs = $programcs.Substring($programcs_header_end, $programcs_footer_start - $programcs_header_end) # extract body
-    $newlines_count_header = $newlines_count_header - 4 # remove the newlines used in the debugging-comment
-    $programcs = "/* DEBUGGING ENABLED:`nThe newlines before the real code are to adjust error-messages`nto reflect the error-positions in your real source-code!`nSo don't remove them or you will get wrong hints.`nExecute 'extract_script.ps1 -Debugging `$false' to remove everything unneeded.*/" + "".PadRight($newlines_count_header, "`n") + $programcs
+    $programcs = "".PadRight($newlines_count_header, "`n") + $programcs
+    Write-Host "WARNING: Debugging has been enabled, which will have a bigger script size, as without it.`
+        If you want to share the final script, deactivate debugging with providing -Debugging `$false."
 } else { # Minimize
     $programcs = $programcs.Substring($programcs_header_end, $programcs_footer_start - $programcs_header_end) # extract body
+    [int] $originalSize = $programcs.Length
     [int] $indention = 8
     [regex] $regex = "(?ms)^[\s]{${indention}}|[\s^`n]+`$|[\s]+[/]{2}[^`n]+"; # search for: multiple new-lines, indentions, spaces at EOL, single-line comments
     $programcs = $regex.Replace($programcs, ""); # remove everything found by the regex above
+    [int] $minimizedSize = $programcs.Length
+    [int] $savedSize = $originalSize - $minimizedSize
+    [float] $savedRatio = $minimizedSize / $originalSize
+    [string] $ratioPercentage = [math]::Round($savedRatio, 4) * 100
+    Write-Host "Release-mode is activated. This will reduce the script-size by removing non-vital data from it.`
+        Original: ${originalSize} bytes, Minimized: ${minimizedSize} bytes, Saved: ${savedSize} bytes, Ratio: ${ratioPercentage} %"
 }
 
 Set-Content -Path ..\Script.cs -Value $programcs
